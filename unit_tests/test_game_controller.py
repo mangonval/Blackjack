ce30862048576bfeb2c9f839c733eeb093bcfa6d
Mario Gonzalez <mangonval@gmail.com>
2021-02-27 12:17:27 -0500

    Added test for calculate score

--- unit_tests/test_game_controller.py
+++ unit_tests/test_game_controller.py
@@ -1,2 +1,12 @@
 import unittest
 
+from game_controller import GameController
+from deck import Deck
+
+
+class TestGameController(unittest.TestCase):
+
+    def test_calculate_score(self):
+        game_controller = GameController()
+        deck = Deck([["A", "Hearts"], ["A", "Hearts"], ["A", "Hearts"], ["A", "Hearts"]])
+        self.assertEqual(game_controller.calculate_score(deck.cards), 14)
