d4f2d44ab3cea224d4293c3f9777bfd0179a614f
Mario Gonzalez <mangonval@gmail.com>
2021-03-20 10:55:29 -0400

    Deleted return_value method from card

--- card.py
+++ card.py
@@ -3,10 +3,4 @@ class Card:
         self.value = value
         self.suit = suit
 
-    def return_card_value(self) -> int:
-        if self.value == "A":
-            return 1
-        elif self.value == "J" or self.value == "Q" or self.value == "K":
-            return 10
-        else:
-            return int(self.value)
+
